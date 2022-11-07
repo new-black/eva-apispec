5a1917aa8c61c99120e15870dd844da7d3343d68
Ruben Oost <ruben.oost@newblack.io>
2022-11-07 15:19:29 +0000

    v2.0.665.10015

--- tools/EVA.SDK.Generator.V2/Commands/Generate/Generator/Outputs/openapi/OpenApiOutput.cs
+++ tools/EVA.SDK.Generator.V2/Commands/Generate/Generator/Outputs/openapi/OpenApiOutput.cs
@@ -95,7 +95,7 @@ public class OpenApiOutput : IOutput
           {
             AuthorizationUrl = new Uri("https://henk2.platform-tools.on-eva.io/ui/auth/azure-connector"),
             TokenUrl = new Uri("https://henk2.platform-tools.on-eva.io/api/auth/azure-connector"),
-            RefreshUrl = null,
+            RefreshUrl = new Uri("https://henk2.platform-tools.on-eva.io/api/auth/azure-connector/refresh"),
             Scopes = new Dictionary<string, string>()
           }
         }
@@ -128,6 +128,26 @@ public class OpenApiOutput : IOutput
     {
       foreach (var eventExportType in input.EventTargets)
       {
+        var properties = new Dictionary<string, OpenApiSchema>
+        {
+          { "UID", new OpenApiSchema { Type = "string" } },
+          { "CreationTime", new OpenApiSchema { Type = "string" } },
+          { "TimeZone", new OpenApiSchema { Type = "string" } },
+          { "Target", new OpenApiSchema { Type = "string" } },
+          { "EventType", new OpenApiSchema { Type = "string" } },
+          { "Identifier", new OpenApiSchema { Type = "string" } },
+          { "BackendID", new OpenApiSchema { Type = "string" } },
+          { "BackendSystemID", new OpenApiSchema { Type = "string" } },
+          { "Region", new OpenApiSchema { Type = "string" } },
+          { "Attempt", new OpenApiSchema { Type = "number" } },
+          { "Environment", new OpenApiSchema { Type = "string" } }
+        };
+
+        if (eventExportType.DataType != null)
+        {
+          properties.Add("Data", new OpenApiSchema { Type = "object" });
+        }
+
         // Render the trigger link
         model.Paths.Add($"/api/azure-connector/subscribe/{eventExportType.Target}/{eventExportType.Type}", new OpenApiPathItem
         {
@@ -137,22 +157,8 @@ public class OpenApiOutput : IOutput
               "x-ms-notification-content", new ExtensionNotification(new OpenApiSchema
               {
                 Type = "object",
-                Properties = new Dictionary<string, OpenApiSchema>
-                {
-                  { "UID", new OpenApiSchema { Type = "string" } },
-                  { "CreationTime", new OpenApiSchema { Type = "string" } },
-                  { "TimeZone", new OpenApiSchema { Type = "string" } },
-                  { "Target", new OpenApiSchema { Type = "string" } },
-                  { "EventType", new OpenApiSchema { Type = "string" } },
-                  { "Identifier", new OpenApiSchema { Type = "string" } },
-                  { "BackendID", new OpenApiSchema { Type = "string" } },
-                  { "BackendSystemID", new OpenApiSchema { Type = "string" } },
-                  { "Region", new OpenApiSchema { Type = "string" } },
-                  { "Attempt", new OpenApiSchema { Type = "number" } },
-                  { "Environment", new OpenApiSchema { Type = "string" } },
-                  { "Data", new OpenApiSchema { Type = "any" } }
-                }
-              })
+                Properties = properties
+              }, $"{eventExportType.Target} {SnakeCaseToPascalCase(eventExportType.Type)}")
             }
           },
           Parameters = new List<OpenApiParameter>
@@ -176,9 +182,9 @@ public class OpenApiOutput : IOutput
             {
               OperationType.Post, new OpenApiOperation
               {
-                Summary = $"EVA Event - {eventExportType.Target} {eventExportType.Type}",
-                Description = $"EVA Event - {eventExportType.Target} {eventExportType.Type}",
-                OperationId = $"AzureConnectorSubscribe{eventExportType.Target}{eventExportType.Type}",
+                Summary = $"EVA Event - {eventExportType.Target} {SnakeCaseToPascalCase(eventExportType.Type)}",
+                Description = $"EVA Event - {eventExportType.Target} {SnakeCaseToPascalCase(eventExportType.Type)}",
+                OperationId = $"AzureConnectorSubscribe{eventExportType.Target}{SnakeCaseToPascalCase(eventExportType.Type)}",
                 Tags = new List<OpenApiTag> { new OpenApiTag { Name = "Technical" } },
                 Extensions = new Dictionary<string, IOpenApiExtension>
                 {
@@ -224,6 +230,24 @@ public class OpenApiOutput : IOutput
                                 }
                               }
                             }
+                          }
+                        }
+                      }
+                    }
+                  }
+                },
+                Responses = new OpenApiResponses
+                {
+                  {
+                    "200", new OpenApiResponse
+                    {
+                      Description = "Default response",
+                      Content = new Dictionary<string, OpenApiMediaType>
+                      {
+                        {
+                          "application/json", new OpenApiMediaType
+                          {
+                            Schema = new OpenApiSchema { Type = "object" }
                           }
                         }
                       }
@@ -454,10 +478,12 @@ public class OpenApiOutput : IOutput
   private class ExtensionNotification : IOpenApiExtension
   {
     private readonly OpenApiSchema _schema;
+    private readonly string? _description;
 
-    public ExtensionNotification(OpenApiSchema schema)
+    public ExtensionNotification(OpenApiSchema schema, string? description = null)
     {
       _schema = schema;
+      _description = description;
     }
 
     public void Write(IOpenApiWriter writer, OpenApiSpecVersion specVersion)
@@ -465,7 +491,19 @@ public class OpenApiOutput : IOutput
       writer.WriteStartObject();
       writer.WritePropertyName("schema");
       _schema.Serialize(writer, specVersion);
+      if (_description != null)
+      {
+        writer.WritePropertyName("description");
+        writer.WriteValue(_description);
+      }
+
       writer.WriteEndObject();
     }
+  }
+
+  private static string SnakeCaseToPascalCase(string s)
+  {
+    var q = s.Split('_').Where(x => !string.IsNullOrWhiteSpace(x)).Select(x => char.ToUpperInvariant(x[0]) + x[1..]);
+    return string.Join("", q);
   }
 }
