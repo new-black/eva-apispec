145b57eecef28c24990f1110cc97c95d3ca1e2fb
Ruben Oost <ruben.oost@newblack.io>
2023-03-17 12:39:30 +0000

    v2.0.680.13720

--- tools/EVA.SDK.Generator.V2/Commands/Generate/Outputs/apidocs/ApiDocsOutput.cs
+++ tools/EVA.SDK.Generator.V2/Commands/Generate/Outputs/apidocs/ApiDocsOutput.cs
@@ -1,5 +1,6 @@
 ﻿using System.Text.Json;
 using System.Text.Json.Serialization;
+using EVA.API.Spec;
 
 namespace EVA.SDK.Generator.V2.Commands.Generate.Outputs.apidocs;
 
@@ -9,6 +10,33 @@ internal class ApiDocsOutput : IOutput<ApiDocsOptions>
   public string[] ForcedRemoves => new string[0];
 
   public async Task Write(OutputContext<ApiDocsOptions> ctx)
+  {
+    // List of all services
+    await GenerateSidebar(ctx);
+
+    // Generate each service
+    foreach (var service in ctx.Input.Services)
+    {
+      await GenerateService(ctx, service);
+    }
+  }
+
+  private static async Task GenerateService(OutputContext<ApiDocsOptions> ctx, ServiceModel service)
+  {
+    var model = new ServiceItem
+    {
+      Name = service.Name
+    };
+
+    var requestType = ctx.Input.Types[service.RequestTypeID];
+    var responseType = ctx.Input.Types[service.ResponseTypeID];
+
+    model.Description = requestType.Description;
+
+    await ctx.Writer.WriteFileAsync($"services/{model.Name}.json", JsonSerializer.Serialize(model, JsonContext.Indented.ServiceItem));
+  }
+
+  private static async Task GenerateSidebar(OutputContext<ApiDocsOptions> ctx)
   {
     var sidebar = new List<SidebarItem>();
 
@@ -33,4 +61,10 @@ internal class SidebarItem
   [JsonPropertyName("id")] public string ID { get; set; }
   [JsonPropertyName("label")] public string Label { get; set; }
   [JsonPropertyName("className")] public string ClassName { get; set; }
+}
+
+internal class ServiceItem
+{
+  [JsonPropertyName("name")] public string Name { get; set; }
+  [JsonPropertyName("description")] public string? Description { get; set; }
 }
--- tools/EVA.SDK.Generator.V2/JsonContext.cs
+++ tools/EVA.SDK.Generator.V2/JsonContext.cs
@@ -15,6 +15,7 @@ namespace EVA.SDK.Generator.V2;
 [JsonSerializable(typeof(HttpInput.EvaResponse))]
 [JsonSerializable(typeof(UpdateCommand.LatestResponse))]
 [JsonSerializable(typeof(SidebarItem[]))]
+[JsonSerializable(typeof(ServiceItem))]
 internal partial class JsonContext : JsonSerializerContext
 {
   internal static JsonContext Indented { get; } = new(new JsonSerializerOptions { WriteIndented = true });
