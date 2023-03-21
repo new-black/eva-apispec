46034c9b07f92dd98a3c986460a2839999d31fc4
Ruben Oost <ruben.oost@newblack.io>
2023-03-21 13:48:49 +0000

    v2.0.680.13778

--- tools/EVA.SDK.Generator.V2/Commands/Generate/Outputs/apidocs/ApiDocsOutput.cs
+++ tools/EVA.SDK.Generator.V2/Commands/Generate/Outputs/apidocs/ApiDocsOutput.cs
@@ -25,15 +25,49 @@ internal class ApiDocsOutput : IOutput<ApiDocsOptions>
   {
     var model = new ServiceItem
     {
-      Name = service.Name
+      Name = service.Name,
+      Request = new ServiceItem.RequestItem
+      {
+        Properties = new List<ServiceItem.PropertyItem>()
+      },
+      Response = new ServiceItem.ResponseItem
+      {
+        Properties = new List<ServiceItem.PropertyItem>()
+      }
     };
 
+    // Request type
     var requestType = ctx.Input.Types[service.RequestTypeID];
-    var responseType = ctx.Input.Types[service.ResponseTypeID];
-
     model.Description = requestType.Description;
 
+    foreach (var (propName, propValue) in requestType.Properties)
+    {
+      model.Request.Properties.Add(new ServiceItem.PropertyItem
+      {
+        Name = propName,
+        Description = propValue.Description,
+        Type = ToTypeName(propValue.Type)
+      });
+    }
+
+    // Response type
+    var responseType = ctx.Input.Types[service.ResponseTypeID];
+    foreach (var (propName, propValue) in responseType.Properties)
+    {
+      model.Response.Properties.Add(new ServiceItem.PropertyItem
+      {
+        Name = propName,
+        Description = propValue.Description,
+        Type = ToTypeName(propValue.Type)
+      });
+    }
+
     await ctx.Writer.WriteFileAsync($"services/{model.Name}.json", JsonSerializer.Serialize(model, JsonContext.Indented.ServiceItem));
+  }
+
+  private static string ToTypeName(TypeReference spec)
+  {
+    return spec.Name;
   }
 
   private static async Task GenerateSidebar(OutputContext<ApiDocsOptions> ctx)
@@ -47,7 +81,8 @@ internal class ApiDocsOutput : IOutput<ApiDocsOptions>
         Type = "doc",
         Label = service.Name,
         ClassName = "api-method post",
-        ID = $"api-reference/{service.Name}"
+        ID = $"api-reference/{service.Name}",
+        Link = $"api-reference/{service.Name}"
       });
     }
 
@@ -59,6 +94,7 @@ internal class SidebarItem
 {
   [JsonPropertyName("type")] public string Type { get; set; }
   [JsonPropertyName("id")] public string ID { get; set; }
+  [JsonPropertyName("link")] public string Link { get; set; }
   [JsonPropertyName("label")] public string Label { get; set; }
   [JsonPropertyName("className")] public string ClassName { get; set; }
 }
@@ -67,4 +103,23 @@ internal class ServiceItem
 {
   [JsonPropertyName("name")] public string Name { get; set; }
   [JsonPropertyName("description")] public string? Description { get; set; }
+  [JsonPropertyName("request")] public RequestItem Request { get; set; }
+  [JsonPropertyName("response")] public ResponseItem Response { get; set; }
+
+  public class RequestItem
+  {
+    [JsonPropertyName("properties")] public List<PropertyItem> Properties { get; set; }
+  }
+
+  public class ResponseItem
+  {
+    [JsonPropertyName("properties")] public List<PropertyItem> Properties { get; set; }
+  }
+
+  public class PropertyItem
+  {
+    [JsonPropertyName("name")] public string Name { get; set; }
+    [JsonPropertyName("description")] public string? Description { get; set; }
+    [JsonPropertyName("type")] public string Type { get; set; }
+  }
 }
