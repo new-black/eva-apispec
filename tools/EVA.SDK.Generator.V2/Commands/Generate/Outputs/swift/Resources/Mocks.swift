1781c53a8a26ff70b8cc522a05117724698aeb3a
toine@springtree.nl <toine@springtree.nl>
2023-07-31 16:51:16 +0200

    fix: make IndirectOptional conform to protocols

--- tools/EVA.SDK.Generator.V2/Commands/Generate/Outputs/swift/Resources/Mocks.swift
+++ tools/EVA.SDK.Generator.V2/Commands/Generate/Outputs/swift/Resources/Mocks.swift
@@ -1,14 +1,18 @@
 ﻿import Foundation
 
-public indirect enum IndirectOptional<T: Codable>: Codable {
+public indirect enum IndirectOptional<T> {
   case none, some(T)
 }
+
+extension IndirectOptional: Codable where T: Codable {}
+extension IndirectOptional: Equatable where T: Equatable {}
+extension IndirectOptional: Hashable where T: Hashable {}
+extension IndirectOptional: Sendable where T: Sendable {}
 
 @frozen public struct EvaAnyCodable: Codable {
 
 }
 
-@frozen
-public enum JSON: Codable, Equatable, Hashable, Sendable {
+@frozen public enum JSON: Codable, Equatable, Hashable, Sendable {
   case null
 }
